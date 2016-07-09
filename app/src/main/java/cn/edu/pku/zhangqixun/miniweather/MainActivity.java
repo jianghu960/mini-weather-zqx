74128dc955af5b51e71ee08a25d2649af7a9ac1b
wuwenchun <496194722@qq.com>
2016-07-09 15:57:57 +0800

    networt permission

---
+++ app/src/main/java/cn/edu/pku/zhangqixun/miniweather/MainActivity.java
@@ -0,0 +1,26 @@
+package cn.edu.pku.zhangqixun.miniweather;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import cn.edu.pku.zhangqixun.util.NetUtil;
+
+/**
+ * Created by zzp on 2016/7/8.
+ */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+        if (NetUtil.getNetworkState(this) != NetUtil.NETWORN_NONE) {
+            Log.d("myWeather", "网络OK");
+            Toast.makeText(MainActivity.this, "网络OK！", Toast.LENGTH_LONG).show();
+        } else {
+            Log.d("myWeather", "网络挂了");
+            Toast.makeText(MainActivity.this, "网络挂了！", Toast.LENGTH_LONG).show();
+        }
+    }
+}
